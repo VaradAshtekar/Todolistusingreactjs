b6787926666df6192fd881ffac1a10370026d70c
VaradAshtekar <varadashtekar96@gmail.com>
2020-10-14 18:27:25 +0530

    Updates

---
+++ precache-manifest.ae267bf86b3dca412458363898515812.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e7203d4c56093cbd921ba8de89146fe",
+    "url": "/Todolistusingreactjs/index.html"
+  },
+  {
+    "revision": "a2734e20f42b13188e2c",
+    "url": "/Todolistusingreactjs/static/css/main.9ddfadaf.chunk.css"
+  },
+  {
+    "revision": "cd8249d3aed6dc2ca5b1",
+    "url": "/Todolistusingreactjs/static/js/2.826712cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todolistusingreactjs/static/js/2.826712cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2734e20f42b13188e2c",
+    "url": "/Todolistusingreactjs/static/js/main.61b03487.chunk.js"
+  },
+  {
+    "revision": "6a25fa2eb4afd5fdb333",
+    "url": "/Todolistusingreactjs/static/js/runtime-main.7231abd2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Todolistusingreactjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
